764f7d6ed1c5bb324bd697f8504fb02d9149fcf9
dietervdbn <dieter.vanderbemden@gmail.com>
2021-04-16 15:54:00 +0200

    pokemon

---
+++ Pokemon/PokemonCard.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pokemon
+{
+    class PokemonCard
+    {
+        private int hitPoints;
+        private int AttackPoints;
+        private int DefPoints;
+        private int SpecialAttack;
+        private int SpecialDefense;
+        private int Speed;
+        private int level = 1;
+        public int HP_Base
+        {
+            get
+            {
+                return hitPoints;
+            }
+            set
+            {
+                hitPoints = value;
+            }
+        }
+        public int Attack_Base
+        {
+            get
+            {
+                return AttackPoints;
+            }
+            set
+            {
+                AttackPoints = value;
+            }
+        }
+        public int Defense_Base
+        {
+            get
+            {
+                return DefPoints;
+            }
+            set
+            {
+                DefPoints = value;
+            }
+        }
+        public int SpecialAttack_Base
+        {
+            get
+            {
+                return SpecialAttack;
+            }
+            set
+            {
+                SpecialAttack = value;
+            }
+        }
+        public int SpecialDefense_Base
+        {
+            get
+            {
+                return SpecialDefense;
+            }
+            set
+            {
+                SpecialDefense = value;
+            }
+        }
+        public int Speed_Base
+        {
+            get
+            {
+                return Speed;
+            }
+            set
+            {
+                Speed = value;
+            }
+        }
+        public string Name { get; set; }
+        public string Type { get; set; }
+        public int Number { get; set; }
+        public int Level
+        {
+            get
+            {
+                return level;
+            }
+            private set
+            {
+                level = value;
+            }
+        }
+        public int Average
+        {
+            get
+            {
+                return ((HP_Base + Attack_Base + Defense_Base + SpecialAttack_Base + SpecialDefense_Base + Speed_Base) / 6);
+            }
+        }
+        public int Total
+        {
+            get
+            {
+                return (HP_Base + Attack_Base + Defense_Base + SpecialAttack_Base + SpecialDefense_Base + Speed_Base);
+            }
+        }
+        public int HP_Full
+        {
+            get
+            {
+                return ((((HP_Base + 50) * Level) / 50) + 10);
+            }
+        }
+        public int Attack_Full
+        {
+            get
+            {
+                return (((Attack_Base * Level) / 50) + 5);
+            }
+        }
+        public int Defense_Full
+        {
+            get
+            {
+                return (((Defense_Base * Level) / 50) + 5);
+            }
+        }
+        public int SpecialAttack_Full
+        {
+            get
+            {
+                return (((SpecialAttack_Base * Level) / 50) + 5);
+            }
+        }
+        public int SpecialDefens_Full
+        {
+            get
+            {
+                return (((SpecialDefense_Base * Level) / 50) + 5);
+            }
+        }
+        public int Speed_Full
+        {
+            get
+            {
+                return (((Speed_Base * Level) / 50) + 5);
+            }
+        }
+        public void LevelUp()
+        {
+                level++;
+                ShowInfo();
+        }
+        public void CreatePokemon(string name, int nummer, int hp, int attack, int def, int spAtt, int spDef, int speed)
+        {
+            Name = name;
+            Number = nummer;
+            HP_Base = hp;
+            Attack_Base = attack;
+            Defense_Base = def;
+            SpecialAttack_Base = spAtt;
+            SpecialDefense_Base = spDef;
+            Speed_Base = speed;
+            Level = 1;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{Name} (level {Level})");
+            Console.WriteLine($"Base stats:");
+            Console.WriteLine($"\t * Health = {HP_Base}");
+            Console.WriteLine($"\t * Speed = {Speed_Base}");
+            Console.WriteLine($"\t * Attack = {Attack_Base}");
+            Console.WriteLine($"\t * Defense = {Defense_Base}");
+            Console.WriteLine($"\t * SpecialAttack = {SpecialAttack_Base}");
+            Console.WriteLine($"\t * SpecialDefens = {SpecialDefense_Base}");
+            Console.WriteLine($"Full stats:");
+            Console.WriteLine($"\t * Health = {HP_Full}");
+            Console.WriteLine($"\t * Speed = {Speed_Full}");
+            Console.WriteLine($"\t * Attack = {Attack_Full}");
+            Console.WriteLine($"\t * Defense = {Defense_Full}");
+            Console.WriteLine($"\t * SpecialAttack = {SpecialAttack_Full}");
+            Console.WriteLine($"\t * SpecialDefens = {SpecialDefens_Full}");
+        }
+    }
+}
---
+++ Pokemon/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace Pokemon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PokemonCard bulbasaur = new PokemonCard();
+            bulbasaur.CreatePokemon("Bulbasaur", 1, 45,49,49,65,65,45);
+
+            PokemonCard charmander = new PokemonCard();
+            charmander.CreatePokemon("Charmander", 4, 39, 52, 43, 60, 50, 65);
+
+            bulbasaur.ShowInfo();
+            LevelPokemon(bulbasaur);
+
+            Battle(bulbasaur, charmander);
+
+
+            static void LevelPokemon(PokemonCard poke)
+            {
+                Console.WriteLine($@"hoeveel keer wil je levellen?");
+                int amount = Convert.ToInt32(Console.ReadLine());
+                for (int i = 0; i < amount; i++)
+                {
+                    poke.LevelUp();
+                }
+            }
+            static int Battle(PokemonCard poke1, PokemonCard poke2)
+            {
+                int result = 0;
+                if (poke1 == null)
+                {
+                    if (poke2 == null)
+                    {
+                        result = 0;
+                        Console.WriteLine($"Bijde pokemons bestaan niet");
+                    }
+                    result = 2;
+                    Console.WriteLine($"De eerste pokemon bestaat niet");
+                }
+                else if (poke2 == null)
+                {
+                    result = 1;
+                    Console.WriteLine($"De 2de pokemon bestaat niet");
+                }
+                else
+                {
+                    int healthPoke1 = poke1.HP_Full -(poke2.Attack_Full - poke1.Defense_Full);
+                    int healthPoke2 = poke2.HP_Full -(poke1.Attack_Full - poke2.Defense_Full);
+
+                    if (healthPoke1 > healthPoke2)
+                    {
+                        Console.WriteLine($"{poke1.Name} wint van {poke2.Name}");
+                        poke1.LevelUp();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{poke2.Name} wint van {poke1.Name}");
+                        poke2.LevelUp();
+                    }
+                }
+                return result;
+            }
+        }
+    }
+}
