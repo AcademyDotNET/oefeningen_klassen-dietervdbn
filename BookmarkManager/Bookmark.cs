4441f2df156db42b16de7f91a92838a8450d5d3f
dietervdbn <dieter.vanderbemden@gmail.com>
2021-04-20 15:42:28 +0200

    book mercker

---
+++ BookmarkManager/Bookmark.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace BookmarkManager
+{
+    class Bookmark
+    {
+        public Bookmark(int nummer)
+        {
+            Console.WriteLine("wat is de naam van de website?");
+            string naam = Console.ReadLine();
+            Console.WriteLine("wat is de url?");
+            string url = Console.ReadLine();
+            Name = naam;
+            Url = url;
+            Nummer = nummer;
+        }
+        private string Name { get; set; }
+        private string Url { get; set; }
+        private int Nummer { get; set; }
+        public static void ToonOverzicht (Bookmark[] websites)
+        {
+            for (int i = 0; i < websites.Length;i++) 
+            {
+                Console.WriteLine($"Website nr: {websites[i].Nummer}");
+                Console.WriteLine($"{websites[i].Name}: {websites[i].Url}");
+                Console.WriteLine();
+            }            
+        }
+        public static void SiteKeuze(Bookmark[] websites, int nummer)
+        {
+            for (int i = 0; i < websites.Length; i++)
+            {
+                if (websites[i].Nummer == nummer)
+                {
+                    Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe", websites[i].Url);
+                }
+            }
+        }
+    }
+}
---
+++ BookmarkManager/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace BookmarkManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //vraag 5 favoriete sites
+            Bookmark[] websites = new Bookmark[5];
+            int nummer = 1;
+            for (int i = 0; i < websites.Length; i++)
+            {
+                Bookmark site = new Bookmark(nummer);
+                websites[i] = site;
+                nummer++;
+            }
+            Bookmark.ToonOverzicht(websites);
+            Console.WriteLine("maak u keuze");
+            int keuze = Convert.ToInt32(Console.ReadLine());
+            Bookmark.SiteKeuze(websites, keuze);
+            
+        }
+    }
+}
